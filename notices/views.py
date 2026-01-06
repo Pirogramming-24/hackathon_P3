670ee7681bdb75f305e17506b9921e90977e3cff
kkw610 <kkw61976@gmail.com>
2026-01-06 13:04:57 +0900

    feat: notice tab에서 작성한 글 수정, 삭제 기능 추가

--- notices/views.py
+++ notices/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
 from piro_sessions.models import Session
 from .models import Notice
 
@@ -22,5 +23,39 @@ def add_notice(request, session_id):
         content=content,
         file=file
     )
+    
+    return redirect(f'/sessioner/{session_id}/?tab=notice')
+
+# ===== 추가: 공지사항 수정 =====
+@require_http_methods(["POST"])
+def edit_notice(request, notice_id):
+    """공지사항 수정"""
+    try:
+        notice = Notice.objects.get(id=notice_id)
+    except Notice.DoesNotExist:
+        return redirect('/')
+    
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    file = request.FILES.get('file')
+    
+    notice.title = title
+    notice.content = content
+    if file:
+        notice.file = file
+    notice.save()
+    
+    return redirect(f'/sessioner/{notice.session.id}/?tab=notice')
+
+# ===== 추가: 공지사항 삭제 =====
+@require_http_methods(["POST"])
+def delete_notice(request, notice_id):
+    """공지사항 삭제"""
+    try:
+        notice = Notice.objects.get(id=notice_id)
+        session_id = notice.session.id
+        notice.delete()
+    except Notice.DoesNotExist:
+        return redirect('/')
     
     return redirect(f'/sessioner/{session_id}/?tab=notice')
